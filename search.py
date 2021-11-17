7ab187960a882de36d137a8aa200b970fdff4d4f
zamoraem <56068057+zamoraem@users.noreply.github.com>
2021-11-17 18:25:23 -0500

    Update search.py

--- search.py
+++ search.py
@@ -5,10 +5,9 @@
 
 import sys
 import json
-import demjson
 import requests
 import wikipedia
-import pullSaidImage as pi
+
 
 
 def display_info(words, location):
@@ -24,34 +23,18 @@ def display_info(words, location):
     #create dictionary to store results for user to access
     r_dict = {'name':words, 'summary':summary}
 
-    # pull image and attach url to r_dict with key: 'image'
-    image_list = get_image(words)
-    r_dict["Images"] = image_list
-
     # write results of name, summary, image to json outfile
     with open(to_file, "w") as outfile:
         json.dump(r_dict, outfile)
     outfile.close()
 
 
-def get_image(search_term):
-    """
-    Takes as input a search term and returns a url corresponding to an image
-    found with the user's query. Uses team member (Laura) microservice
-    """
-    to_file = 'C:\\Users\\elain\\361\\search\\CS361_search\\TEST_JSON.json'
-    pi.pullSaidImage_give(search_term, to_file)
-    with open(to_file, 'r') as openfile:
-        json_obj = json.load(openfile)
-    json_final = demjson.decode(json_obj)
-    return json_final
-
 
 def main():
-    # Note: You can use the following to simply call the function with search terms as args
-    #     : user query must be entered with spaces between terms
-    user_query = 'nelson mandela'
-    display_info(user_query, 'C:\\Users\\elain\\361\\search\\CS361_search\\search_result.txt')
+    # example usage
+   # user_query = 'nelson mandelma'
+   # file_location = 'C:\\Users\\elain\\361\\search_result.txt'
+   # display_info(user_query, file_location)
 
-if __name__ == "__main__":
-        main()
+#if __name__ == "__main__":
+#        main()
